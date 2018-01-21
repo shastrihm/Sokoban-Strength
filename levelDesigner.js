ec2c75afc1364182ecac4f6932b4635872a93ef7
shastrihm <32887722+shastrihm@users.noreply.github.com>
2018-01-21 15:17:52 -0800

    UI Improvements

--- levelDesigner.js
+++ levelDesigner.js
@@ -9,38 +9,9 @@ function userSetUpCanvas()
 {
   canvas.width = size*Number(document.getElementById("columns").value);
   canvas.height = size*Number(document.getElementById("rows").value);
-
+  redraw();
   clearInput("columns");
   clearInput("rows");
-}
-
-
-
-function setUpWhatHere(what,id)
-{
-  let coords=String(document.getElementById(id).value);
-  let col = Number(coords.split(",")[0]), row=Number(coords.split(",")[1]);
-
-  if(col<=canvas.width/size && row<=canvas.height/size)
-  {
-    function findBoulderAt(boulder){
-      return (boulder.x == boulder.width*(col-1)) && (boulder.y == boulder.height*(row-1))
-    } 
-
-    let doesExist = boulderArray.find(findBoulderAt);
-    let testObj = {
-                    width: size,
-                   height: size,
-                        x: size*(col-1),
-                        y: size*(row-1)
-                   };
-    if(doesExist===undefined && !collision(char, testObj)){
-      new what(col,row);
-      redraw();
-    }
-  }
-
-  clearInput(id);
 }
 
 
@@ -50,29 +21,6 @@ function undoLastBoulder()
   boulderArray.pop();
   redraw();
 }
-
-
-
-function removeBoulderAt()
-{
-  function remove(array, element){
-      return array.filter(e=>e!==element);
-  }
-
-  let coords=String(document.getElementById("removeBoulderHere").value);
-  let col = Number(coords.split(",")[0]), row=Number(coords.split(",")[1]);
-
-  function findBoulderAt(boulder){
-    return (boulder.x == boulder.width*(col-1)) && (boulder.y == boulder.height*(row-1))
-  }
-
-  let thisOne = boulderArray.find(findBoulderAt);
-  boulderArray = remove(boulderArray,thisOne);
-  redraw();
-
-  clearInput("removeBoulderHere");
-}
-
 
 
 
@@ -93,3 +41,82 @@ function setUpCharHere()
 
   clearInput("placeCharHere");
 }
+
+
+var what;
+function drawWhatOnClick(e)
+{
+  function onCanvasClick(e) 
+  {
+      //alert(getCursorPosition(e));
+      testCoords = getCursorPosition(e);
+        for(var i=0; i<canvas.width/size; i++)
+        {
+          if(testCoords[0]>=size*i && testCoords[0]<=size*(i+1)){
+            var col=i+1;
+            break
+          }
+        }
+
+        for(var j=0; j<canvas.height/size; j++)
+        {
+          if(testCoords[1]>=size*j && testCoords[1]<=size*(j+1)){
+            var row=j+1;
+            break
+          }
+        }
+
+        function findBoulderAt(boulder){
+          return (boulder.x == boulder.width*(col-1)) && (boulder.y == boulder.height*(row-1))
+        } 
+
+        if(what==Boulder || what==Wall)
+        {
+          let doesExist = boulderArray.find(findBoulderAt);
+          let testObj = {
+                          width: size,
+                         height: size,
+                              x: size*(col-1),
+                              y: size*(row-1)
+                         };
+          if(doesExist===undefined && !collision(char, testObj)){
+            new what(col,row);
+            redraw();
+          }
+        }
+      
+        else if(what=="remove")
+        {
+          function remove(array, element){
+            return array.filter(e=>e!==element);
+          }
+          let thisOne = boulderArray.find(findBoulderAt);
+          boulderArray = remove(boulderArray,thisOne);
+          redraw();
+        }
+  //code goes here
+  } 
+   
+  function getCursorPosition(e) 
+  {
+      let x;
+      let y;
+      if (e.pageX != undefined && e.pageY != undefined) {
+      x = e.pageX;
+      y = e.pageY;
+      }
+      else {
+      x = e.clientX + document.body.scrollLeft +
+              document.documentElement.scrollLeft;
+      y = e.clientY + document.body.scrollTop +
+              document.documentElement.scrollTop;
+      }
+      x -= canvas.offsetLeft;
+      y -= canvas.offsetTop;
+      
+      return [x,y];
+  }
+  canvas.addEventListener("click", onCanvasClick, false); 
+}
+
+
