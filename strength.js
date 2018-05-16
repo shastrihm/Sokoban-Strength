8e06498577d850b9c75307955c60d3e3dbc1f1b9
shastrihm <32887722+shastrihm@users.noreply.github.com>
2018-05-15 19:02:40 -0700

    Simplified movement, pushing mechanic

--- strength.js
+++ strength.js
@@ -5,7 +5,7 @@ var pushUp,pushDown,pushLeft,pushRight;
 var boulderArray = [];
 var grid=false;
 var size=60;
-var c=67; 
+var storePos;
 
 var faceDown = new Image();
 faceDown.src = "faceDown.png";
@@ -20,19 +20,28 @@ var wallImg = new Image();
 wallImg.src = "Wall.png";
 var boulderImg = new Image();
 boulderImg.src = "Boulder.png";
+var flagImg = new Image();
+flagImg.src = "flag.png";
 
 
 function Character(column,row)
 { 
-  
   this.width = size;
   this.height = size;
-  this.x = this.width*(column-1);
-  this.y = this.height*(row-1);
+  this.col = column;
+  this.row = row; 
+  this.x = this.width*(this.col-1);
+  this.y = this.height*(this.row-1);
   this.img = faceDown;
-  this.speed = 2.5;
+  this.speed = size;
+
+  this.updatePos = function(){
+    this.x = this.width*(this.col-1);
+    this.y = this.height*(this.row-1);
+  }
 
   this.draw = function(){
+    this.updatePos();
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
@@ -42,12 +51,20 @@ function Wall(column,row)
 { 
   this.width = size;
   this.height = size;
-  this.x = this.width*(column-1);
-  this.y = this.height*(row-1);
+  this.col = column;
+  this.row = row; 
+  this.x = this.width*(this.col-1);
+  this.y = this.height*(this.row-1);
   this.img = wallImg;
   boulderArray.push(this);
 
+  this.updatePos = function(){
+    this.x = this.width*(this.col-1);
+    this.y = this.height*(this.row-1);
+  }
+
   this.draw = function(){
+    this.updatePos();
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
@@ -63,60 +80,61 @@ function Boulder(column, row)
       return array.filter(e=>e!==element);
     }
 
-    if(keys[c]){
-      //top
-      if(pushUp && this.y>=0)
-      {
-        this.y -= this.height;
-        if(this.y>=0 && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
-          this.draw();}
-        else{this.y+=this.height;}
-      } 
-      //bottom
-      if(pushDown && this.y+this.height<=canvas.height)
-      {
-        this.y += this.height;
-        if(this.y+this.height<=canvas.height && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
-          this.draw();}
-        else{this.y-=this.height;}
-      }
-      //left
-      if(pushLeft && this.x >=0)
-      {
-        this.x -= this.width;
-        if(this.x>=0 && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
-          this.draw();}
-        else{this.x+=this.width;}
-      }
-      //right
-      if(pushRight && this.x +this.width <= canvas.width)
-      {
-        this.x += this.width;
-        if(this.x+this.width<=canvas.width && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
-          this.draw();}
-        else{this.x-=this.width;}
-      }
-      //top 
-      if(!pushUp && !pushLeft && !pushRight)
-      {
-        ctx.clearRect(this.x, this.y-this.height, this.width, this.height);
-      }
-      //bottom
-      if(!pushDown && !pushLeft && !pushRight)
-      {
-        ctx.clearRect(this.x, this.y+this.height, this.width, this.height);
-      }
-      //left
-      if(!pushLeft && !pushUp && !pushDown)
-      {
-        ctx.clearRect(this.x-this.width, this.y, this.width, this.height);
-      }
-      //right
-      if(!pushRight && !pushUp && !pushDown)
-      {
-        ctx.clearRect(this.x+this.width, this.y, this.width, this.height);
-      }
+    
+    //top
+    if(pushUp && this.row>1)
+    {
+      this.row--;
+      console.log('hi');
+      if(this.y>=0 && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
+        this.draw();}
+      else{this.row++;}
+    } 
+    //bottom
+    if(pushDown && this.row<canvas.height/size)
+    {
+      this.row++;
+      if(this.y+this.height<=canvas.height && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
+        this.draw();}
+      else{this.row--;}
     }
+    //left
+    if(pushLeft && this.col>1)
+    {
+      this.col--;
+      if(this.x>=0 && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
+        this.draw();}
+      else{this.col++}
+    }
+    //right
+    if(pushRight && this.col<canvas.width/size)
+    {
+      this.col++;
+      if(this.x+this.width<=canvas.width && !remove(boulderArray,this).some((boulder)=>collision(this,boulder))){
+        this.draw();}
+      else{this.col--;}
+    }
+    //top 
+    if(!pushUp && !pushLeft && !pushRight)
+    {
+      ctx.clearRect(this.x, this.y-this.height, this.width, this.height);
+    }
+    //bottom
+    if(!pushDown && !pushLeft && !pushRight)
+    {
+      ctx.clearRect(this.x, this.y+this.height, this.width, this.height);
+    }
+    //left
+    if(!pushLeft && !pushUp && !pushDown)
+    {
+      ctx.clearRect(this.x-this.width, this.y, this.width, this.height);
+    }
+    //right
+    if(!pushRight && !pushUp && !pushDown)
+    {
+      ctx.clearRect(this.x+this.width, this.y, this.width, this.height);
+    }
+    
   }
 
 }
@@ -135,23 +153,25 @@ function loadInitial() {
 var d=68, s=83, w=87, a=65 
 function move() {//doesn't check for collisions between boulders
   //d
+
   if (keys[d]) {
     if (char.x + char.speed + char.width <= canvas.width) {
       ctx.clearRect(char.x, char.y, char.width, char.height);
-      char.x += char.speed;
+      storePos = [char.col,char.row];
+      char.col++;
 
       if(!boulderArray.some(boulder=>collision(char, boulder))){
         char.img = faceRight;
         redraw();
       }
       else{
-        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder))
+        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder));
         if(thisBoulder instanceof Boulder)
         {
           thisBoulder.animateBoulderPush();
         }
         ctx.clearRect(char.x, char.y, char.width, char.height);
-        char.x -= char.speed;
+        char.col--;
         char.img = faceRight;
         redraw();
       }
@@ -161,20 +181,21 @@ function move() {//doesn't check for collisions between boulders
   if (keys[s]) {
     if (char.y + char.speed + char.width <= canvas.height) {
       ctx.clearRect(char.x, char.y, char.width, char.height);
-      char.y += char.speed;
+      storePos = [char.col,char.row];
+      char.row++;
 
       if(!boulderArray.some(boulder=>collision(char, boulder))){
         char.img = faceDown;
         redraw();
       }
       else{
-        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder))
+        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder));
         if(thisBoulder instanceof Boulder)
         {
           thisBoulder.animateBoulderPush();
         }
         ctx.clearRect(char.x, char.y, char.width, char.height);
-        char.y -= char.speed;
+        char.row--;
         char.img = faceDown;
         redraw();
       }
@@ -184,20 +205,21 @@ function move() {//doesn't check for collisions between boulders
   if (keys[w]) {
     if (char.y - char.speed >= 0) {
       ctx.clearRect(char.x, char.y, char.width, char.height);
-      char.y -= char.speed;
+      storePos = [char.col,char.row];
+      char.row--;
 
       if(!boulderArray.some(boulder=>collision(char, boulder))){
         char.img = faceUp;
         redraw();
       }
       else{
-        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder))
+        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder));
         if(thisBoulder instanceof Boulder)
         {
           thisBoulder.animateBoulderPush();
         }
         ctx.clearRect(char.x, char.y, char.width, char.height);
-        char.y += char.speed;
+        char.row++;
         char.img = faceUp;
         redraw();
       } 
@@ -207,19 +229,21 @@ function move() {//doesn't check for collisions between boulders
   if (keys[a]) {
     if (char.x - char.speed >= 0) {
       ctx.clearRect(char.x, char.y, char.width, char.height);
-      char.x -= char.speed;
+      storePos = [char.col,char.row];
+      char.col--;
+
       if(!boulderArray.some(boulder=>collision(char, boulder))){
         char.img = faceLeft;
         redraw();
       }
       else{
-        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder))
+        let thisBoulder=boulderArray.find(boulder=>collision(char,boulder));
         if(thisBoulder instanceof Boulder)
         {
           thisBoulder.animateBoulderPush();
         }
         ctx.clearRect(char.x, char.y, char.width, char.height); 
-        char.x+=char.speed; 
+        char.col++;
         char.img = faceLeft;
         redraw();
       }
@@ -244,67 +268,31 @@ function redraw()
 }
 
 
-function collision(ent1, ent2) //rectangle-rectangle collsion
-{ 
-  if (ent1.x < ent2.x + ent2.width &&
-   ent1.x + ent1.width > ent2.x &&
-   ent1.y < ent2.y + ent2.height &&
-   ent1.height + ent1.y > ent2.y) 
+function collision(ent1, ent2) 
+{ //ent1 is usually the character
+  if (ent1 instanceof Character && ent1.row === undefined)
+  {
+    return false;
+  }
+  else if (ent1.col == ent2.col && ent1.row == ent2.row) 
   {
     // collision detected
-      whichSide(ent1, ent2);
-      
+    whichSide(ent1, ent2);
     return true;
   }
   else{return false;}
 }
 
-function whichSide(A, B) //computes Minkowski Sum
+function whichSide(ent1, ent2) //which direction boulder push?
 { 
-  var w = 0.5 * (A.width + B.width);
-  var h = 0.5 * (A.height + B.height);
-  var dx = ((A.width-A.x)/2) - ((B.width-B.x)/2);
-  var dy = ((A.height-A.y)/2) - ((B.height-B.y)/2);
-      
-      var wy = w * dy;
-      var hx = h * dx;
+  if(!(ent1 instanceof Character)){return}
 
-      if (wy > hx)
-      {
-          if (wy > -hx && keys[s])
-          {
-              /* collision at the top of obstacle */
-              console.log('top');
-              pushDown = true;
-              pushUp = pushLeft = pushRight = false;
-          }
-          else if(keys[a])
-          {
-            /* on the right of obstacle */ 
-            console.log('left')
-            pushLeft = true;
-            pushUp = pushDown = pushRight = false;
-          }
-      }
-      else
-      {
-          if (wy > -hx && keys[d])
-          {
-            /* on the left of obstacle */
-            console.log('right'); 
-            pushRight = true;
-            pushUp = pushLeft = pushDown = false;
-          }
-             
-          else if(!keys[d] && !keys[a])
-          {
-           /* at the bottom of obstacle*/
-           console.log('bottom'); 
-           pushUp = true;
-           pushDown = pushLeft = pushRight = false;
-          }
-              
-      } 
+  let charCol = storePos[0];
+  let charRow = storePos[1];
+  pushUp = (charCol==ent2.col && charRow==ent2.row+1);
+  pushDown = (charCol==ent2.col && charRow==ent2.row-1);
+  pushRight = (charCol==ent2.col-1 && charRow==ent2.row);
+  pushLeft = (charCol==ent2.col+1 && charRow==ent2.row);
 }
 
 function showGrid()
@@ -337,6 +325,7 @@ function playGame()
 
   document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
+    setTimeout(function(){keys[e.keyCode] = false},10)
   });
   document.body.addEventListener("keyup", function(e) {
     keys[e.keyCode] = false;
@@ -352,5 +341,4 @@ function playGame()
 }
 
 playGame();
-
 
