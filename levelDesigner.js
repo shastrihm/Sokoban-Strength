082fd0d2751d1573217ca5c8a24f78ba29690a7e
shastrihm <32887722+shastrihm@users.noreply.github.com>
2018-06-22 19:22:13 -0700

    Update levelDesigner.js

--- levelDesigner.js
+++ levelDesigner.js
@@ -4,7 +4,6 @@ function clearInput(id)
   input.value ="";
 }
 
-
 function userSetUpCanvas()
 {
   canvas.width = size*Number(document.getElementById("columns").value);
@@ -13,15 +12,6 @@ function userSetUpCanvas()
   clearInput("columns");
   clearInput("rows");
 }
-
-
-
-function undoLastBoulder()
-{
-  boulderArray.pop();
-  redraw();
-}   
-
 
 
 function setUpCharHere(col,row)
@@ -50,28 +40,66 @@ function setDestination(col,row)
   }
 }
 
-var what;
-function drawBoulders(col,row)
+function setBoulderOrWall(boulderOrWall,col,row)
 {
-  function findBoulderAt(boulder){
-    return (boulder.x == boulder.width*(col-1)) && (boulder.y == boulder.height*(row-1))
-  } 
+  let doesExist = boulderArray.find(boulder=>findObsAt(boulder,col,row));
+  let testObj = {
+                  width: size,
+                 height: size,
+                      col: col,
+                      row: row,
+                      x: size*(col-1),
+                      y: size*(row-1)
+                 };
 
+  if(doesExist===undefined && 
+    !collision(char, testObj) && 
+    !collision(endgoal, testObj))
+  {
+    new boulderOrWall(col,row);
+    redraw();
+  } 
+}
+
+function setIce(col,row)
+{
+  let doesExist = iceArray.find(tile=>findObsAt(tile,col,row));
+  if(doesExist===undefined)
+  {
+    new Ice(col,row);
+    redraw();
+  }
+}
+
+function setWormhole(col,row)
+{
+  let doesExist = wormholeArray.find(hole=>findObsAt(hole,col,row));
+  if(doesExist===undefined)
+  {
+    new Wormhole(col,row);
+    redraw();
+  }  
+}
+
+function clearAt(col,row)
+{
+  for(let arr in allArrays)
+  {
+    let thisOne = allArrays[arr].find(elm=>findObsAt(elm,col,row));
+    if(thisOne !== undefined)
+    {
+      // need remove element in place
+      removeInPlace(allArrays[arr],thisOne)      
+    }
+    redraw();
+  }
+}
+
+function setObjects(col,row)
+{ 
   if(what==Boulder || what==Wall)
   {
-    let doesExist = boulderArray.find(findBoulderAt);
-    let testObj = {
-                    width: size,
-                   height: size,
-                        col: col,
-                        row: row,
-                        x: size*(col-1),
-                        y: size*(row-1)
-                   };
-    if(doesExist===undefined && !collision(char, testObj)){
-      new what(col,row);
-      redraw();
-    }
+    setBoulderOrWall(what,col,row);
   }
 
   else if (what==Character)
@@ -81,53 +109,179 @@ function drawBoulders(col,row)
 
   else if(what=="remove")
   { 
-    if(endgoal.col == col && endgoal.row == row){
-      endgoal = new Destination();
-      return;
-    }
-    else if(char.col == col && char.row == row){
-      char = new Character();
-      return;
-    }
-    function remove(array, element){
-      return array.filter(e=>e!==element);
-    }
-    let thisOne = boulderArray.find(findBoulderAt);
-    boulderArray = remove(boulderArray,thisOne);
-    redraw();
+    clearAt(col,row)
   }
 
   else if(what==Destination)
   {
     setDestination(col,row);
   }
+
+  else if(what==Ice)
+  {
+    setIce(col,row);
+  }
+
+  else if(what==Wormhole)
+  {
+    setWormhole(col,row);
+  }
+
 }
 
+function anchorLineAtClick(clickCol,clickRow)
+{
+  
+  let fromx = (size*(clickCol-1))+(size*0.5);
+  let fromy = (size*(clickRow-1))+(size *0.5);
 
+  return [fromx,fromy]
+  // drawLine(fromx,fromy,)
+  // canvas.addEventListener("click", pausable(onCanvasClickDrawItem), false); 
+  // canvas.addEventListener("mousemove", pausable(highlightSquare), false);
+}
+
+function followLineToMouse(mouseCol,mouseRow)
+{
+  let tox = (size*(mouseCol-1))+(size*0.5);
+  let toy = (size*(mouseRow-1))+(size *0.5);
+
+  return [tox,toy]
+}
+
+function rawLineCoordsToColRow(x,y)
+{
+  return [((x-(size*0.5))/size)+1,((y-(size*0.5))/size)+1]
+}
 
 function drawWhatOnClick(e)
 {
-  
+  function drawTransparentImg(img, opacity, col, row)
+  { 
+    redraw();
+
+    for(let arr in allArrays)
+    {
+      if(boulderArray.some(entity=>findObsAt(entity,col,row)))
+      {
+        return;
+      }
+    }
+
+    ctx.globalAlpha = opacity;
+    ctx.drawImage(img,size*(col-1), size*(row-1), size, size);
+    ctx.globalAlpha = 1;
+  }
+
   function highlightSquare(e)
   {
     testCoords = getCursorPosition(e);
-    var col=testCoords[0], row=testCoords[1];
+    let col=testCoords[0], row=testCoords[1];
 
     ctx.clearRect(0,0,canvas.width, canvas.height)
     redraw();
-    ctx.fillStyle = "rgba(224,252,68,0.3)";
-    ctx.fillRect(size*(col-1), size*(row-1), size, size);
+
+    let opacity = 0.5;
+    switch(what)
+    {
+      case Boulder:
+        drawTransparentImg(boulderImg, opacity, col, row);
+        break;
+      case Wall:
+        drawTransparentImg(wallImg, opacity, col, row);
+        break;
+      case Ice:
+        drawTransparentImg(iceImg, opacity, col, row);
+        break;
+      case Wormhole:
+        drawTransparentImg(wormhole_inactiveImg, opacity, col, row);
+        break;
+      case Character:
+        drawTransparentImg(faceDown, opacity, col, row);
+        break;
+      case Destination:
+        drawTransparentImg(flagImg, opacity, col, row);
+        break;
+      case "remove":
+        let redRGBa = "rgba(218,31,31,0.3)"
+        ctx.fillStyle = redRGBa;
+        ctx.fillRect(size*(col-1), size*(row-1), size, size);
+        break;
+      default:
+        let whiteRGBa = "rgba(155,155,155,0.3)"
+        ctx.fillStyle = whiteRGBa;
+        ctx.fillRect(size*(col-1), size*(row-1), size, size);
+
+    }
+
   }
 
+  var listenerRefs = {};
+  function drawAnchoredLine(e)
+  {
+
+    freezeInput();
+
+    function getMouseMovePosition(e,from)
+    {   
+      return function(e)
+        {
+          what = null;
+          testCoords = getCursorPosition(e)
+          let col=testCoords[0], row=testCoords[1];
+          let to = anchorLineAtClick(col,row);
+          redraw();
+          drawLineWithCircle(from[0],from[1],to[0],to[1])          
+        }      
+    }
+
+    function getMouseUpPosition(e,from,thisHole)
+    { 
+      return function(e)
+      {
+        
+        testCoords = getCursorPosition(e);
+        let col=testCoords[0], row=testCoords[1];
+        
+        let checkCoords = rawLineCoordsToColRow(from[0],from[1]);
+        //console.log(checkCoords[0],checkCoords[1],col,row);
+
+        if(wormholeArray.some(hole=>findObsAt(hole,col,row))
+          && !(checkCoords[0] == col && checkCoords[1] == row))
+        {
+          let thatHole = wormholeArray.find(hole=>findObsAt(hole,col,row));
+          //console.log(thatHole.warpto)
+          thisHole.linkTo(thatHole); 
+        }
+        canvas.removeEventListener('mousemove',listenerRefs.mousemove, false);
+        canvas.removeEventListener('mouseup',listenerRefs.mouseup,false);
+      }
+    }
+
+    testCoords = getCursorPosition(e);
+    let col=testCoords[0], row=testCoords[1];
+    let downflag = false;
+    if(wormholeArray.some(hole=>collision(hole,{col:testCoords[0],row:testCoords[1]})))
+    {
+      let thisHole = wormholeArray.find(hole=>findObsAt(hole,col,row));
+      //console.log(thisHole);
+      let from = anchorLineAtClick(col,row);
+      listenerRefs.mousemove = getMouseMovePosition(e,from);
+      listenerRefs.mouseup = getMouseUpPosition(e,from,thisHole);
+
+      canvas.addEventListener('mousemove',listenerRefs.mousemove, false);
+      canvas.addEventListener('mouseup', listenerRefs.mouseup, false);
+    }
+
+    unfreezeInput();
+  }
 
   function onCanvasClickDrawItem(e) 
   {
     //alert(getCursorPosition(e));
     testCoords = getCursorPosition(e);
-    var col=testCoords[0], row=testCoords[1];
-
-    drawBoulders(col,row);
-    
+    let col=testCoords[0], row=testCoords[1];
+    setObjects(col,row);
     //code goes here
   } 
    
@@ -169,52 +323,71 @@ function drawWhatOnClick(e)
   }
 
   canvas.addEventListener("click", pausable(onCanvasClickDrawItem), false); 
-  canvas.addEventListener("mousemove", highlightSquare, false);
+  canvas.addEventListener("mousemove", pausable(highlightSquare), false);
+  canvas.addEventListener("mousedown", drawAnchoredLine, false);
+
 }
 
-
-function exportConfig(canvas,boulderArray,char,endgoal)
+//make sure wormhole warpto are accounted for
+function exportConfig(canvas,allArrays)
 {
-  let exportThis = [];
+  let exportThis = {};
+  let specificObjs;
   let numcols = canvas.width/size;
   let numrows = canvas.height/size;
-  exportThis.push(String(numcols) + "x" + String(numrows) + "|");
-  for (let boulder in boulderArray)
-  {
-    boul = boulderArray[boulder];
-    exportThis.push(boul.parent, boul.col,boul.row,"|");
+  exportThis["dims"] = [numcols,numrows];
+  for (let arr in allArrays)
+  { 
+    specificObjs = {};
+    let thisArr = allArrays[arr];
+    if(thisArr.length>0 && thisArr[0].col !== undefined)
+    { 
+      let name = thisArr[0].parent;
+      exportThis[name] = [];
+    
+      if(name=="Wormhole")
+      {
+        thisArr.forEach(thing=>exportThis[name].push([thing.col,thing.row,thing.warpto.col,thing.warpto.row]));
+      }
+      else
+      {
+        thisArr.forEach(thing=>exportThis[name].push([thing.col,thing.row]));
+      }
+    }
   }
-  exportThis.push(char.parent,char.col,char.row,"|");
-  exportThis.push(endgoal.parent,endgoal.col,endgoal.row,"|");
-  return String(exportThis);
+  return JSON.stringify(exportThis);
 }
 
 
 function importConfig(config)
-{
-  //config is a string 
-  boulderArray = [];
-  let itemArray = config.trim().split("|").filter(item => item!="");
+{ 
+  emptyNestedArraysInPlace(allArrays);
+  //config is a JSON string 
+  let itemArray = JSON.parse(config);
+  let vals;
+  let col,row;
+  for(let name in itemArray)
+  { 
+    vals = itemArray[name];
 
-  for(let i in itemArray)
-  {
-    if(i==0)
+    if(name=="dims")
     {
-      let dims = itemArray[i].split("x");
-      let cols = Number(dims[0]);
-      let rows = Number(dims[1]);
-      canvas.width = cols*size;
-      canvas.height = rows*size;
+      canvas.width = vals[0]*size;
+      canvas.height = vals[1]*size;
     }
 
     else
     {
-      filt1 = itemArray[i].split(",").filter(item => item!="");
-      let crtInstance = filt1[0];
-      let col = Number(filt1[1]);
-      let row = Number(filt1[2]);
+      for(let i in vals)
+      {
+        let coords = vals[i];
+        col = coords[0];
+        row = coords[1];
+        warptocol = coords[2];
+        warptorow = coords[3];
+        (masterObstacles[name])[0](col,row,warptocol,warptorow);           
+      }
 
-      (masterObstacles[crtInstance])[0](col,row);   
     }
     
   }
@@ -239,7 +412,7 @@ function validateConfig()
 
 function getConfig()
 {
-  document.getElementById("I/O_config").value = exportConfig(canvas,boulderArray,char,endgoal);
+  document.getElementById("I/O_config").value = exportConfig(canvas,allArrays);
 }
 
 
